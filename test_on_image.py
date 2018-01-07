1891263a4afbc4ba771d9d0fe9f7d7a7de323d02
Kaustubh Devkar <kdevkar1998@gmail.com>
2018-01-07 23:47:12 +0530

    Extracted digit from image

--- test_on_image.py
+++ test_on_image.py
@@ -2,7 +2,7 @@ import numpy as np,cv2,imutils
 from sklearn.externals import joblib
 
 
-img = cv2.imread('test2.jpg')
+img = cv2.imread('sample_image.jpg')
 
 img = imutils.resize(img,width=300)
 cv2.imshow("Original",img)
@@ -26,22 +26,22 @@ ret,cnts,hie = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX
 model = joblib.load('model.pkl')
 for c in cnts:
     try:
-        mask = np.zeros(gray.shape,dtype="uint8")   #empty mask for each iteration
+        mask = np.zeros(gray.shape,dtype="uint8")   
 
         (x,y,w,h) = cv2.boundingRect(c)
         
         hull = cv2.convexHull(c)
-        cv2.drawContours(mask,[hull],-1,255,-1)     #draw hull on mask
-        mask = cv2.bitwise_and(thresh,thresh,mask=mask) #segment digit from thresh
+        cv2.drawContours(mask,[hull],-1,255,-1)    
+        mask = cv2.bitwise_and(thresh,thresh,mask=mask) 
 
-        digit = mask[y-8:y+h+8,x-8:x+w+8]       #just for better approximation
-        digit = cv2.resize(digit,(28,28))
-        digit = np.array(digit)
+        roi = mask[y-9:y+h+9,x-9:x+w+9]       
+        roi = cv2.resize(roi,(28,28))
+        roi = np.array(roi)
 
-        digit = digit.reshape(-1,784)
-        label = model.predict(digit)
+        roi = roi.reshape(1,784)
+        prediction = model.predict(roi)
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),1)
-        cv2.putText(img,str(int(label)),(x+2,y-5),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0,255,0),2)
+        cv2.putText(img,str(int(prediction)),(x+2,y-5),cv2.FONT_HERSHEY_SIMPLEX,1.0,(0,255,0),2)
     except Exception as e:
         print(e)
         pass
