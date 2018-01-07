8d2c1091c902e7c474b8a6576f28f1f8710d974f
Kaustubh Devkar <kdevkar1998@gmail.com>
2018-01-08 00:18:54 +0530

    Adding images to test

--- test_on_image.py
+++ test_on_image.py
@@ -2,7 +2,7 @@ import numpy as np,cv2,imutils
 from sklearn.externals import joblib
 
 
-img = cv2.imread('sample_image.jpg')
+img = cv2.imread('sum1.jpg')
 
 img = imutils.resize(img,width=300)
 cv2.imshow("Original",img)
@@ -10,7 +10,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray Image",gray)
 
 
-kernel = np.ones((20,20),np.uint8)
+kernel = np.ones((40,40),np.uint8)
 
 
 blackhat = cv2.morphologyEx(gray,cv2.MORPH_BLACKHAT,kernel)
@@ -34,19 +34,19 @@ for c in cnts:
         cv2.drawContours(mask,[hull],-1,255,-1)    
         mask = cv2.bitwise_and(thresh,thresh,mask=mask) 
 
-        roi = mask[y-9:y+h+9,x-9:x+w+9]       
+        roi = mask[y-7:y+h+7,x-7:x+w+7]       
         roi = cv2.resize(roi,(28,28))
         roi = np.array(roi)
 
         roi = roi.reshape(1,784)
         prediction = model.predict(roi)
-        probability = model.predict_proba(roi)
-        print(probability)
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),1)
-        cv2.putText(img,str(int(prediction)),(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,255,0),1)
-        cv2.putText(img,str((max(probability[0])*100))+"%",(x,y+h),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),1)
+    
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
+        cv2.putText(img,str(int(prediction)),(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,0,0),1)
+        
     except Exception as e:
         print(e)
         pass
+img = imutils.resize(img,width=500)
 cv2.imshow('Detection',img)
                
